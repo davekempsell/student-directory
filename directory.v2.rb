cfa798c53f05299ccfbdf5d613b5bed51d74daf3
Dave Kempsell <dave_kempsell@hotmail.com>
2022-05-12 09:00:30 +0000

    refactored inter_menu

--- directory.v2.rb
+++ directory.v2.rb
@@ -1,18 +1,16 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To end please press enter twice"
-
-  students = []
-
+  
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :November}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  
-  students
 end
 
 def print_header
@@ -20,46 +18,46 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if students.count > 0
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do 
-    puts "1. Input the students"
-    puts "2. Show the students list"
-    puts "9. Exit"
-  
-    selection = gets.chomp
-  
-    case selection
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students list"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
     when '1'
-      students = input_students
+      input_students
     when '2'
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when '9'
       exit
     else
       puts "Please select a valid option"
-    end
+  end
+end
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
   end
 end
 
 interactive_menu
-
-
-
-
-
-
